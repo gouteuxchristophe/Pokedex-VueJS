b0a1632f8678c1ef888b3dcf34ef9f8cf549d512
gouteuxchristophe <chris.gouteux@outlook.fr>
2023-04-25 17:52:50 +0200

    modif config css

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import {useStore } from 'vuex'
 import { defineComponent } from 'vue'
 import type { Pokemon } from './stores/vuex'
 import '@/styles/globals.css';
+import '@/input.css';
 
 export default defineComponent ({
   components: {
